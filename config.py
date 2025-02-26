ee192b6e572411c3b5f9a934e91ce95331ec259e
encr23m10040 <yuri@encar.com>
2025-02-26 16:07:31 +0900

    250226 test07 검수탭 추가 - 1차

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 
 # 작업 : 배열 처리
 # 작업 : browser 쪽 해당 페이지에서 변수 처리
+# 작업 : 딜러 계정 변수화 01, 07 페이지 import
 
-car_numbers = ["338가1607"]
-car_type = "수입"
+car_numbers = ["197하9878"]
+car_type = "국산"
 owner = "채유리"
